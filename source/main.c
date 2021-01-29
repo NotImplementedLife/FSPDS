9aa61ff2adc813b1ae03a49c01ce06c31a9427bf
NotImplementedLife <not.implemented.life@gmail.com>
2021-01-29 15:06:28 +0200

    added border

--- source/main.c
+++ source/main.c
@@ -12,6 +12,8 @@ int filescount=0;
 char files[MAXFILESCOUNT][29];
 char sizes[MAXFILESCOUNT][7];
 
+PrintConsole consoleBG,console;
+
 void long_to_size_string(char dest[7],long sz)
 {
     dest[5]='B';
@@ -48,7 +50,7 @@ void long_to_size_string(char dest[7],long sz)
     int r=((sz&0x3FF)*100)>>10;
     if(q>=10)
     {
-        iprintf("Error: size limit exceeded. \n");
+        iprintf("Fatal error: size limit exceeded. \n");
         exit(-1);
     }
     if(r>=100) r/=10;
@@ -98,14 +100,115 @@ void loadFiles()
     }
 }
 
+void c_cls()
+{
+    //iprintf("\e[1;1H\e[2J");
+    iprintf("\x1b[2J");
+}
+
+void c_goto(int row, int column)
+{
+    printf("\033[%d;%dH", row, column);
+}
+
+void writeFrame()
+{
+    int i;
+    iprintf("\x1b[33m");
+    c_goto(0,0);
+    iprintf("\024");
+    for(int i=1;i<31;i++) iprintf("\004");
+    iprintf("\022");
+    for(int i=1;i<23;i++)
+    {
+        c_goto(i,0); iprintf("\003");
+        c_goto(i,31); iprintf("\003");
+    }
+    c_goto(23,0);
+    iprintf("\025");
+    for(int i=1;i<31;i++) iprintf("\004");
+    iprintf("\023");
+    c_goto(23,0);
+    //for(i=32;i--;) iprintf("\x02");
+}
+
+void writeEntry(int i,int listpos)
+{
+    consoleSelect(&consoleBG);
+    for(int i=0;i<3;i++)
+    {
+        c_goto(1+3*listpos+i,1);
+        for(int j=0;j<30;j++) iprintf("\x1b[33m\x02");
+    }
+    consoleSelect(&console);
+    c_goto(1+3*listpos,2);
+    iprintf("\x1b[39m%s",files[i]);
+    c_goto(2+3*listpos,20);
+    iprintf("\x1b[39m%s",sizes[i]);
+    //iprintf("  %s\n\t\t\t\t\t\t\t%s\n",files[i],sizes[i]);
+}
+
+void init()
+{
+    //thanks: https://github.com/DS-Homebrew/GodMode9i/blob/master/arm9/source/main.cpp
+    videoSetModeSub(MODE_0_2D);
+	vramSetBankH(VRAM_H_SUB_BG);
+	consoleInit(&consoleBG, 1, BgType_Text4bpp, BgSize_T_256x256, 7, 0, false, true);
+	consoleInit(&console, 0, BgType_Text4bpp, BgSize_T_256x256, 15, 0, false, true);
+
+    // overwrite character fonts
+	dmaFillWords(0xFFFFFFFF, (void*)0x6200040, 32);
+    dmaFillWords(0x00F00F00, (void*)0x6200060, 32);
+
+    dmaFillWords(0x00000000, (void*)0x6200080, 8);
+    dmaFillWords(0xFFFFFFFF, (void*)0x6200088, 4);
+    dmaFillWords(0x00000000, (void*)0x620008C, 8);
+    dmaFillWords(0xFFFFFFFF, (void*)0x6200094, 4);
+    dmaFillWords(0x00000000, (void*)0x6200098, 8);
+
+    dmaFillWords(0x00000000, (void*)0x6200240, 8);
+    dmaFillWords(0x00FFFFFF, (void*)0x6200248, 4);
+    dmaFillWords(0x00F00000, (void*)0x620024C, 8);
+    dmaFillWords(0x00F00FFF, (void*)0x6200254, 4);
+    dmaFillWords(0x00F00F00, (void*)0x6200258, 8);
+
+    dmaFillWords(0x00F00F00, (void*)0x6200260, 8);
+    dmaFillWords(0x00F00FFF, (void*)0x6200268, 4);
+    dmaFillWords(0x00F00000, (void*)0x620026C, 8);
+    dmaFillWords(0x00FFFFFF, (void*)0x6200274, 4);
+    dmaFillWords(0x00000000, (void*)0x6200278, 8);
+
+    dmaFillWords(0x00000000, (void*)0x6200280, 8);
+    dmaFillWords(0xFFFFFF00, (void*)0x6200288, 4);
+    dmaFillWords(0x00000F00, (void*)0x620028C, 8);
+    dmaFillWords(0xFFF00F00, (void*)0x6200294, 4);
+    dmaFillWords(0x00F00F00, (void*)0x6200298, 8);
+
+    dmaFillWords(0x00F00F00, (void*)0x62002A0, 8);
+    dmaFillWords(0xFFF00F00, (void*)0x62002A8, 4);
+    dmaFillWords(0x00000F00, (void*)0x62002AC, 8);
+    dmaFillWords(0xFFFFFF00, (void*)0x62002B4, 4);
+    dmaFillWords(0x00000000, (void*)0x62002B8, 8);
+
+	BG_PALETTE[ 0]=BG_PALETTE_SUB[ 0]=0x7FFF;
+    BG_PALETTE[63]=BG_PALETTE_SUB[63]=0x01DF;
+}
+
 int main(int argc, char **argv)
 {
-    consoleDemoInit();
-    iprintf("Loading...\n");
-    loadFiles();
+    init();
+    //loadFiles();
+
+    writeFrame();
+    //c_cls();
+    filescount=2;
+    strcpy(files[0],"000000_0000000000000_000.ppm");
+    strcpy(sizes[0]," 127KB");
+    strcpy(files[1],"000001_0000000000000_001.ppm");
+    strcpy(sizes[1],"   13B");
     for(int i=0;i<filescount;i++)
     {
-        iprintf("  %s\n\t\t\t\t\t\t\t%s\n",files[i],sizes[i]);
+        writeEntry(i,i);
     }
 
 	while(1) {
